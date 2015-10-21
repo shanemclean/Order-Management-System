7cc5be8f15753fed353333e22f85c588c43bd61f
shanemclean <shane.g.mclean@gmail.com>
2015-10-20 23:31:25 -0500

    Added Customer Domain

---
+++ src/OrderManagementSystem.Repository/Domain/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Repository.Domain
+{
+    public class Customer : BaseEntity<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Telephone { get; set; }
+        public string Email { get; set; }
+        public int Balance { get; set; }
+    }
+}
